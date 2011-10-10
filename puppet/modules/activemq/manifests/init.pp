cdaa8c9e18410b3e2ab45f4b0f00423a656c9ce7
Rob LaRubbio <rob@larubbio.org>
2011-10-10 16:03:01 -0700

    Update activemq config

--- puppet/modules/activemq/manifests/init.pp
+++ puppet/modules/activemq/manifests/init.pp
@@ -1,29 +1,17 @@
 class activemq {
+  include java::open_jdk
 
-  $tar_source = '/root/tama-config/puppet/modules/activemq/files/apache-activemq-5.5.0-bin.tar.gz'
 
-  exec { "activemq_untar":
-    command => "tar xf ${tar_source}",
-    cwd     => "/opt",
-    creates => "/opt/apache-activemq-5.5.0",
-    path    => ["/bin",],
-  }
-  
-  exec { "rename_activemq_dir":
-    command => "mv apache-activemq-5.5.0 activemq",
-    cwd => '/opt',
-    path => ["/bin",],
-    require => Exec["activemq_untar"],
-    creates => '/opt/activemq',
+  package { "activemq":
+    ensure    => "present",
+    require   => Package["java-1.6.0-openjdk"]
   }
 
   file { "/etc/init.d/activemq":
     owner  => root,
     group  => root,
     mode   => 755,
-    #source => "puppet://${servername}/activemq/activemq-init.d",
-    source => "/root/tama-config/puppet/modules/activemq/files/activemq-init.d",
-    require => Exec["activemq_untar"],
+    content => puppet:///modules/activemq/activemq-init.d,
   }
 
   service { "activemq":
@@ -31,6 +19,6 @@ class activemq {
     enable     => true,
     hasrestart => true,
     hasstatus  => true,
-    require => File["/etc/init.d/activemq"],
+    require => [Package["activemq"], File["/etc/init.d/activemq"]],
   }
 }
--- puppet/modules/java/manifests/init.pp
+++ puppet/modules/java/manifests/init.pp
@@ -1,12 +1,7 @@
-class java {
-  exec { "sun_jre_6":
-    path => ["/usr/bin","/bin","/usr/sbin","/sbin"],
-    cwd => "/usr/local",
-    command => "wget http://download.oracle.com/otn-pub/java/jdk/6u27-b07/jre-6u27-linux-i586.bin -O jre6.bin \
-		&& chmod +x jre6.bin  \
-		&& ./jre6.bin \
-		&& ln -s /usr/local/jre1.6.0_27/bin/java /usr/bin/java \
-		&& rm -f jre6.bin",
-    creates => "/usr/local/jre1.6.0_27/bin/java",
+class java::open_jdk {
+  #include repos::jpackage
+
+  package { "java-1.6.0-openjdk":
+    ensure  =>  "present"
   }
 }
